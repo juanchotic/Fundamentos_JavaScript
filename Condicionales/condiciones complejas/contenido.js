6ff3afc62b2983b9b015bd60c28d22fa33c5bdf7
Juan Fernando <you@example.com>
2021-01-08 00:20:49 -0500

    Trabajando con condiciones complejas

---
+++ Condicionales/condiciones complejas/contenido.js
@@ -0,0 +1,12 @@
+var balance = 5000;
+
+if (balance >= 0){
+   alert("El balance es positivo");
+   if (balance > 10000){
+   alert("El balance es muy grande")
+   }
+} else {
+ alert("El balance es Negativo");
+}
+
+

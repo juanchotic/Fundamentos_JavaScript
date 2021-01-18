388a11d3e6b6cdd81d5c41a4ac39160c9930826c
Juan Fernando Cataño Higuita <56710089+juanchotic@users.noreply.github.com>
2021-01-18 01:42:07 -0500

    Trabajando con Matrices

---
+++ 10matrices/content.js
@@ -0,0 +1,6 @@
+var valoresMultiples = [] ; // Es una Coleción
+valoresMultiples[0] = 50;
+valoresMultiples[1] = 60;
+valoresMultiples[2] = "Hola";
+
+alert(`La Matriz a llamar es ${valoresMultiples[2]}`);

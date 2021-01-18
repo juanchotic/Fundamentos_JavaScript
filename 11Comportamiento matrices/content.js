4784735c6e64f25fe00f96a325ffd58cc208700f
Juan Fernando Cataño Higuita <56710089+juanchotic@users.noreply.github.com>
2021-01-18 01:53:41 -0500

    Comportamiento de las Matrices

---
+++ 11Comportamiento matrices/content.js
@@ -0,0 +1,16 @@
+var miCadena = "Esta es una frase simple";
+alert(miCadena.length);
+
+var miColeccion = [10,20,"test",true,99];
+alert(miColeccion.length); // 0 1 2 3 4 long = 5
+
+// METODOS DE LAS COLECCIONES
+
+miCadena.toUpperCase();
+miColeccion.reverse(); // Dar la vuelta a la Matriz
+miColeccion.sort(); // Reordena alfabeticamente los elementos
+miColeccion.pop(); // Toma el ultimo valor y se lo asigna 
+miColeccion.push(123); // Añade el elemento a la colección
+
+
+// Manual donde se explique los elementos.

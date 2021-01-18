d31ba147171fd4709d448d70f727c3524be27ca6
Juan Fernando Cataño Higuita <56710089+juanchotic@users.noreply.github.com>
2021-01-18 02:14:59 -0500

    Iterando en las colecciones - EJERCICIO

---
+++ 13IteracionMatrices2/content.js
@@ -0,0 +1,12 @@
+var coleccion = [500,500,500,500,500,500,500];
+
+var total = 0;
+
+for (var i = 0; i < coleccion.length; i++){
+    // Añadir el valor actual al total
+    total = total + coleccion[i];
+}
+
+// Una vez salimos del bucle
+
+alert('el valor total es: '+total);

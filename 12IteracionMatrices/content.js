5d5bab2019ee6ed1860868f202d15b1bfb958127
Juan Fernando Cataño Higuita <56710089+juanchotic@users.noreply.github.com>
2021-01-18 02:05:12 -0500

    Iterando en las colecciones - Recorrido en un ARRAY

---
+++ 12IteracionMatrices/content.js
@@ -0,0 +1,10 @@
+var myArray = [];
+myArray = [10,20,"test",true,99]
+var i = 0; // Indice que empieza en 0
+
+while (i < myArray.length) { // Comprobar condicion
+
+    alert("El valor es:"+myArray[i]);
+    // Alert usa el indice para acceder al elemento actual
+    i++; // Incremento del indice
+}
